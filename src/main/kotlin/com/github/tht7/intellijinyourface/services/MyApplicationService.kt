8fbbb48f70cae068e5b7ed5b1bd50385882e3a43
GitHub Action <action@github.com>
2022-09-22 08:44:20 +0000

    Template cleanup

--- src/main/kotlin/com/github/tht7/intellijinyourface/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/tht7/intellijinyourface/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.tht7.intellijinyourface.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.tht7.intellijinyourface.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/tht7/intellijinyourface/services/MyProjectService.kt
+++ src/main/kotlin/com/github/tht7/intellijinyourface/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.tht7.intellijinyourface.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.tht7.intellijinyourface.MyBundle
 
 class MyProjectService(project: Project) {
 
